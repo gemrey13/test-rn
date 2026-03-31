48990daf1b8678048158132b89637c73909002ba
gemrey13 <gemreyranola@gmail.com>
2026-03-31 12:27:59 +0800

    Add new tabs

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+
+const Insights = () => {
+    return (
+        <View>
+            <Text>Insights Page!</Text>
+        </View>
+    )
+};
+
+
+export default Insights;
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+
+const Settings = () => {
+    return (
+        <View>
+            <Text>Settings Page!</Text>
+        </View>
+    )
+};
+
+
+export default Settings;
